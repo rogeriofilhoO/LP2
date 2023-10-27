f2edf982d5ffa02ff0f615c02e764270b6254b84
crosssmart <rogerio.rhty@gmail.com>
2023-10-27 10:09:35 -0400

    jtf

---
+++ BI2/aula-3-(PHP)/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php   
+        echo "Olá, eu sou um script PHP!"
+    ?>
+</body>
+</html>
+
+<!-- Para utilizar tem que ter na pasata do XAMPP no C: ai
+colocar uma pasta la com index.php 
+eai colocar no navergador "localhost" e colocar depois da barra o nome da pasta para que ele
+possa pegar o arquivo e mostrar a página :) -->
